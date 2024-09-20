66c0779c986ec2e181a6801944a7d2a511b249b4
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-09-20 13:53:38 -0400

    cp

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -5,9 +5,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error('POSTGRES_URL environment variable is not set');
+if (!process.env.SUPABASE_POSTGRES_URL) {
+  throw new Error('SUPABASE_POSTGRES_URL environment variable is not set');
 }
 
-export const client = postgres(process.env.POSTGRES_URL);
+export const client = postgres(process.env.SUPABASE_POSTGRES_URL);
 export const db = drizzle(client, { schema });
