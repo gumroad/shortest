e1fbb4f9be4a325cf6b3d7dece0305cc76422a9d
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-09-24 13:55:14 -0400

    cp

--- app/api/generate-tests/route.ts
+++ app/api/generate-tests/route.ts
@@ -1,4 +1,4 @@
-import { openai } from "@ai-sdk/openai";
+import { anthropic } from '@ai-sdk/anthropic';
 import { streamObject } from "ai";
 import { z } from "zod";
 import { GenerateTestsInput } from "./schema";
@@ -15,15 +15,22 @@ export async function POST(req: Request) {
       : "Update existing test files in order to get the PR build back to passing. Make updates to tests solely, do not add or remove tests."
   }
 
+  PR Diff:
+  <PR Diff>
   ${pr_diff}
+  </PR Diff>
 
   Existing test files:
+  <Test Files>
   ${test_files
     .map((file) => `${file.name}${file.content ? `: ${file.content}` : ""}`)
-    .join("\n")}`;
+    .join("\n")}
+  </Test Files>`;
+
+  console.log("Prompt: ", prompt);
 
   const { elementStream } = await streamObject({
-    model: openai("gpt-4-turbo"),
+    model: anthropic("claude-3-5-sonnet-20240620"),
     output: "array",
     schema: z.object({
       name: z.string(),
@@ -32,10 +39,14 @@ export async function POST(req: Request) {
     prompt,
   });
 
+  console.log("Element stream: ", elementStream);
+
   const tests = [];
   for await (const test of elementStream) {
     tests.push(test);
   }
+
+  console.log("Tests: ", tests);
 
   return new Response(JSON.stringify({ tests }), {
     headers: { "Content-Type": "application/json" },
