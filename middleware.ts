26bd3a8f2e859b292ee31145e905698bfac365b7
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-09-27 16:47:12 -0400

    protect route

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/api/generate-tests(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/api/generate-tests(.*)"],
 };
