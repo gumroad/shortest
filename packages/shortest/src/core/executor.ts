7c7c7b126a2e4ea6d63f0f0f446c1b5fd8c1fd70
Mohammad Rad <argo.mohrad@gmail.com>
2024-10-31 17:41:30 -0700

    feat(executor): Add test suite execution and reporting

--- packages/shortest/src/core/executor.ts
+++ packages/shortest/src/core/executor.ts
@@ -1,18 +1,35 @@
 import { Reporter } from './reporter';
 import { TestCompiler } from './compiler';
+import { TestParser } from './parser';
+import { initialize } from '../index';
 
 export class TestExecutor {
   private reporter: Reporter;
   private compiler: TestCompiler;
+  private parser: TestParser;
 
   constructor() {
     this.reporter = new Reporter();
     this.compiler = new TestCompiler();
+    this.parser = new TestParser();
   }
 
   async executeTest(file: string) {
+    await initialize();
+    
     this.reporter.startFile(file);
-    this.reporter.reportTest('Sample test');
+    
+    const compiledPath = await this.compiler.compileFile(file);
+    const module = await import(compiledPath);
+    const suites = await this.parser.parseModule(module);
+    
+    for (const suite of suites) {
+      this.reporter.startSuite(suite.name);
+      
+      for (const test of suite.tests) {
+        this.reporter.reportTest(test.testName, 'passed');
+      }
+    }
   }
 
   getReporter(): Reporter {
