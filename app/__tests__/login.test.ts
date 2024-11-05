e68eddda6dfa56e04462f5a5a6c4b3d652c6f593
Mohammad Rad <argo.mohrad@gmail.com>
2024-11-04 23:34:33 -0800

    Chore: udpate

--- app/__tests__/login.test.ts
+++ app/__tests__/login.test.ts
@@ -8,13 +8,14 @@ interface LoginState {
 define('Validate login feature implemented with Clerk', () => {
 
   new UITestBuilder<LoginState>('/')
-    .test('Login to the app using Github account')
-    .given('username and password', { username: 'test', password: 'test' })
-    .expect('should successfully redirect to /dashboard')
+    .test('Login to the app using Email and Password')
+    .given('username and password', { username: 'argo.mohrad@gmail.com', password: 'Shortest1234' })
+    .when('Logged in, click on view dashboard button')
+    .expect('should successfully redirect to /')
 
-    new UITestBuilder<LoginState>('/')
-    .test('Login to the app using Google account')
-    .given('username and password', { username: 'test', password: 'test' })
-    .expect('should successfully redirect to /dashboard')
+    // new UITestBuilder<LoginState>('/')
+    // .test('Login to the app using Google account')
+    // .given('username and password', { username: 'test', password: 'test' })
+    // .expect('should successfully redirect to /dashboard')
 
 });
