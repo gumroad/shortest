663255a0179f67ce0b32665dd129b97c1bb8d6f4
Mohammad Rad <argo.mohrad@gmail.com>
2024-11-14 20:10:06 -0800

    Update: add github login testing

--- app/__tests__/login.test.ts
+++ app/__tests__/login.test.ts
@@ -7,15 +7,15 @@ interface LoginState {
 
 define('Validate login feature implemented with Clerk', () => {
 
-  new UITestBuilder<LoginState>('/')
-    .test('Login to the app using Email and Password')
-    .given('username and password', { username: 'argo.mohrad@gmail.com', password: 'Shortest1234' })
-    .when('Logged in, click on view dashboard button')
-    .expect('should successfully redirect to /')
+  // new UITestBuilder<LoginState>('/')
+    // .test('Login to the app using Email and Password')
+    // .given('username and password', { username: 'argo.mohrad@gmail.com', password: 'Shortest1234' })
+    // .when('Logged in, click on view dashboard button')
+    // .expect('should successfully redirect to /')
 
-    // new UITestBuilder<LoginState>('/')
-    // .test('Login to the app using Google account')
-    // .given('username and password', { username: 'test', password: 'test' })
-    // .expect('should successfully redirect to /dashboard')
+    new UITestBuilder<LoginState>('/')
+    .test('Login to the app using Github login')
+    .given('Github username and password', { username: `${process.env.GITHUB_USERNAME}`, password: `${process.env.GITHUB_PASSWORD}` })
+    .expect('should successfully redirect to /dashboard')
 
 });
