5866162ee653795db7d6d8a529024f6409634f19
Mohammad Rad <argo.mohrad@gmail.com>
2024-11-16 00:42:18 -0800

    Update

--- app/__tests__/home.test.ts
+++ app/__tests__/home.test.ts
@@ -9,6 +9,7 @@ define('Validate Dasboard is accessible by users', () => {
   new UITestBuilder<loginButton>('/')
     .test('Validate that users can access the dashboard')
     .given('baseUrl', { url: 'http://localhost:3000' })
-    .expect('Should redirect to /dashboard and see the dashboard')
+    .when('Clicking on view dashboard button')
+    .expect('Should be able redirect to /dashboard and see the dashboard')
 
 });
