36c4fd46fdcc0c5fd2872bfc67d036e4d862e007
Mohammad Rad <argo.mohrad@gmail.com>
2024-11-21 01:40:18 -0800

    refactor: update test files to use new assertion syntax

--- app/__tests__/home.test.ts
+++ app/__tests__/home.test.ts
@@ -4,12 +4,10 @@ interface loginButton {
   url: string;
 }
 
-define('Validate Dasboard is accessible by users', () => {
-
+define('Validate Dasboard is accessible by users', async () => {
   new UITestBuilder<loginButton>('/')
     .test('Validate that users can access the dashboard')
     .given('baseUrl', { url: 'http://localhost:3000' })
     .when('Clicking on view dashboard button')
-    .expect('Should be able redirect to /dashboard and see the dashboard')
-
+    .expect('Should be able redirect to /dashboard and see the dashboard');
 });
