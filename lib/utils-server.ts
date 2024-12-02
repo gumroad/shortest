e30b343476d58947118e4ec4d4e01d88b6e3706f
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-01 18:17:11 -0800

    update: add await to headers

--- lib/utils-server.ts
+++ lib/utils-server.ts
@@ -1,7 +1,7 @@
 import { headers } from "next/headers"
 
-export function baseUrl() {
-  const headersList = headers()
+export async function baseUrl() {
+  const headersList = await headers()
   const host = headersList.get('host')
   const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https'
   return `${protocol}://${host}`
