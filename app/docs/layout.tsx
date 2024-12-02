0ef584291d10a050eec6e312ed7409686a9273d4
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-01 18:52:04 -0800

    fix layout

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -6,9 +6,11 @@ export default function DocsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex items-start gap-8">
-      <Leftbar key="leftbar" />
-      <div className="flex-[5.25]">{children}</div>
+    <div className="sm:container mx-auto w-[90vw]">
+      <div className="flex items-start gap-8">
+        <Leftbar key="leftbar" />
+        <div className="flex-[5.25]">{children}</div>
+      </div>
     </div>
   );
 }
