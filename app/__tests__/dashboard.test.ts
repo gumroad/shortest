af55fab41eb5d114ea4bab9afcfee68117fd036a
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-06 02:57:24 -0800

    remove whitespace

--- app/__tests__/dashboard.test.ts
+++ app/__tests__/dashboard.test.ts
@@ -1,11 +1,9 @@
 import { test } from '@antiwork/shortest';
 
-
 const githubCredentials = {
     username: process.env.GITHUB_USERNAME,
     password: process.env.GITHUB_PASSWORD,
 }
   
 test('Login to the app using Github login', githubCredentials)
-
 test('clicking write new test button should initiate test generation')
