76b15707c80d8fc6410e4c802390fcc3f9dc398e
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-06 01:34:25 -0800

    update

--- app/__tests__/login.test.ts
+++ app/__tests__/login.test.ts
@@ -1,7 +1,6 @@
 import { test } from '@antiwork/shortest';
 
 const githubCredentials = {
-  email: process.env.GITHUB_EMAIL || 'argo.mohrad@gmail.com',
   username: process.env.GITHUB_USERNAME,
   password: process.env.GITHUB_PASSWORD,
 }
