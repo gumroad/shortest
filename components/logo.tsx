753e11c882ea13808bf2010c2d80464a4dbc9c99
Razvan Marescu <razvan@marescu.net>
2024-12-13 19:58:50 -0800

    Add logo

---
+++ components/logo.tsx
@@ -0,0 +1,6 @@
+export const Logo = ({ className = "", fill = "currentColor" }: { className?: string, fill?: string }) => (
+  <svg viewBox="0 0 500 800" fill="red" xmlns="http://www.w3.org/2000/svg" className={className}>
+    <path d="M0 413V325L500 519V607L0 800V712L402.081 562.974L0 413Z" fill={fill}/>
+    <path d="M500 88L97.9186 237.974L500 387V475L0 282V194L500 0V88Z" fill={fill}/>
+  </svg>
+);
