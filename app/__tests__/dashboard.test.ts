2dfc2baa14a15e6e3ece3fcd122877d98df84523
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-20 03:14:25 -0800

    use clerk test for login to dashboard '

--- app/__tests__/dashboard.test.ts
+++ app/__tests__/dashboard.test.ts
@@ -1,4 +1,21 @@
-import { shortest } from '@antiwork/shortest';
-  
-shortest('Login to the app using Github login', { username: process.env.GITHUB_USERNAME, password: process.env.GITHUB_PASSWORD });
+import { shortest } from "@antiwork/shortest";
+import { clerk, clerkSetup } from "@clerk/testing/playwright";
+
+let frontendUrl = process.env.PLAYWRIGHT_TEST_BASE_URL ?? "http://localhost:3001";
+
+shortest.beforeAll(async ({ page }) => {
+  await clerkSetup({
+    frontendApiUrl: frontendUrl,
+  });
+  await clerk.signIn({
+    page,
+    signInParams: {
+      strategy: "email_code",
+      identifier: "shortest+clerk_test@example.com",
+    },
+  });
+
+  await page.goto(frontendUrl + "/dashboard");
+});
+
 shortest('clicking write new test button should initiate test generation');
