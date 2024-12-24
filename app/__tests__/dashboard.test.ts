eb6b4685a2ccb53b6b55b5c4b5b7ff0bb353b58c
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-24 10:06:42 -0800

    update tests

--- app/__tests__/dashboard.test.ts
+++ app/__tests__/dashboard.test.ts
@@ -3,19 +3,14 @@ import { clerk, clerkSetup } from "@clerk/testing/playwright";
 
 let frontendUrl = process.env.SHORTEST_TEST_BASE_URL ?? "http://localhost:3000";
 
-shortest.beforeAll(async ({ page }) => {
-  await clerkSetup({
-    frontendApiUrl: frontendUrl,
-  });
-  await clerk.signIn({
-    page,
-    signInParams: {
-      strategy: "email_code",
-      identifier: "shortest+clerk_test@example.com",
-    },
-  });
 
-  await page.goto(frontendUrl + "/dashboard");
-});
+shortest('Login to the app using email and password', {
+  username: process.env.GITHUB_USERNAME,
+  password: process.env.GITHUB_PASSWORD
+})
 
 shortest('Verify that the user can access the /dashboard page');
+
+shortest("Verify that users can generate test cases")
+.expect("It takes up to 20 seconds for the tests to be generated")
+.expect("You should validate that the vitest tests are generated and shown in the UI")
