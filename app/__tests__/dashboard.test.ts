73606488aff44ae2faebacef4b0c4208923a5f92
Mohammad Rad <argo.mohrad@gmail.com>
2024-12-27 13:19:23 -0800

    update the test email

--- app/__tests__/dashboard.test.ts
+++ app/__tests__/dashboard.test.ts
@@ -1,6 +1,6 @@
 import { shortest } from "@antiwork/shortest";
 
 shortest(
-  "Login to the App using magic link. Use this email: 'stay-ants@taoyeoeo.mailosaur.net'",
+  "Login to the App using magic link. Use this email: 'mission-health@rdt7stzf.mailosaur.net'",
 );
 shortest("Verify that the user can access the /dashboard page");
