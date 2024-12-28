8be65b235a4c9c3fd352b088a7645a1b3c5e4f02
Tarun Teja Jangila <taruntejajangila@gmail.com>
2024-12-29 01:39:01 +0530

    removing

--- components/Favicon.tsx
+++
@@ -1,8 +0,0 @@
-Hi Ande!
-Eroju mana Madhapur lo unna yummy yummy Tree Arabian Food Court ki vacham ande. Eroju maa maridi garu thana birthday party kosam mamalni ee mandi restaurant ki teesukuni vacharu. Morning intlo chala specials prepare chesukuni tinesariki chala late ayyindi ande.
-
-Restaurant ambience chala bagundi ande—calm ga, neat ga, chala pleasant ga undhi. Dining experience enjoy chesamu. So, ekkuva tinakudadhu ani, 4 members ki 1/2 mandi and two starters order chesi tinaleka tinesam ande.
-
-Mandi taste entha adbhutam ga undho, bill kuda alane undhandi! 😅
-
-Repu malli inkoka interesting video tho kaluddam ande. Appatvaraku do like, share, and subscribe to my channel! Mee support naaku chala important ande. Bye! 👋
