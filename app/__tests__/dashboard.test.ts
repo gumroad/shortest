ab6e35b34df9d1096e84c3eb709863205144c590
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-12-31 12:28:01 -0500

    cp

--- app/__tests__/dashboard.test.ts
+++ app/__tests__/dashboard.test.ts
@@ -1,6 +1,4 @@
 import { shortest } from "@antiwork/shortest";
 
-shortest(
-  "Login to the App using magic link. Use this email: 'mission-health@rdt7stzf.mailosaur.net'",
-);
+shortest("Login using this email: mission-health@rdt7stzf.mailosaur.net");
 shortest("Verify that the user can access the /dashboard page");
