18881bc7b67b0557090f4f2331c084d0023aac9b
Sahil Lavingia <sahil.lavingia@gmail.com>
2025-01-01 21:31:17 -0500

    cp

--- app/(dashboard)/page.test.ts
+++ app/(dashboard)/page.test.ts
@@ -1,5 +1,5 @@
 import { shortest } from "@antiwork/shortest";
 
-shortest("Verify that buttons are rounded on the landing page");
+shortest("Verify that buttons on the landing page are rounded");
 shortest("Login using this email: mission-health@rdt7stzf.mailosaur.net");
 shortest("Verify that the user can access the /dashboard page");
