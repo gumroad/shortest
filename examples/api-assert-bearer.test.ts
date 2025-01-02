3e3ee9e857c5a9e98057e72cabcf1d2d0cc1b6ad
gladysh <gladyshinbox@outlook.com>
2025-01-02 11:51:41 +0100

    remove vars from test

--- examples/api-assert-bearer.test.ts
+++ examples/api-assert-bearer.test.ts
@@ -2,15 +2,12 @@ import { shortest } from "@antiwork/shortest";
 import { ALLOWED_TEST_BEARER, TESTING_API_BASE_URI } from "@/lib/constants";
 
 // @note you should be authenticated in Clerk to run this test
-const missingBearerToken = `
+shortest(`
   Test the API POST endpoint ${TESTING_API_BASE_URI}/assert-bearer with body { "flagged": "false" } without providing a bearer token.
   Expect the response to indicate that the token is missing
-`;
+`);
 
-const validBearerToken = `
+shortest(`
   Test the API POST endpoint ${TESTING_API_BASE_URI}/assert-bearer with body { "flagged": "true" } and the bearer token ${ALLOWED_TEST_BEARER}.
   Expect the response to show "flagged": true
-`;
-
-shortest(missingBearerToken);
-shortest(validBearerToken);
+`);
