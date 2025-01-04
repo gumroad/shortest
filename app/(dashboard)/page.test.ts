bc0e7cb45cc35c27c8955937821fa1c8f4485596
Razvan Marescu <razvan@marescu.net>
2025-01-04 05:19:41 -0800

    f

--- app/(dashboard)/page.test.ts
+++ app/(dashboard)/page.test.ts
@@ -1,7 +1,6 @@
 import { shortest } from "@antiwork/shortest";
 
-const loginEmail = `shortest@${process.env.MAILOSAUR_SERVER_ID}.mailosaur.net`;
-if (!loginEmail) throw new Error("MAILOSAUR_LOGIN_EMAIL is required");
-
 shortest("Verify that buttons on the landing page are rounded");
+
+const loginEmail = `shortest@${process.env.MAILOSAUR_SERVER_ID}.mailosaur.net`;
 shortest("Log in", { email: loginEmail });
